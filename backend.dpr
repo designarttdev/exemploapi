cc3c40db985e5a98ece2073db118a4491aa87079
Luiz Carlos <designarttdev@gmail.com>
2021-06-24 15:36:53 -0300

    V3
    Version 3

--- backend.dpr
+++ backend.dpr
@@ -5,13 +5,38 @@ program backend;
 {$R *.res}
 
 uses
-  System.SysUtils;
+var Users: TJSONArray;
 
 begin
-  try
-    { TODO -oUser -cConsole Main : Insert code here }
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
+  THorse.Use(Jhonson());
+
+  Users := TJSONArray.Create;
+
+  THorse.Get('/users',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send<TJSONAncestor>(Users.Clone);
+    end);
+
+  THorse.Post('/users',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      User : TJSONObject;
+    begin
+      User := Req.Body<TJSONObject>.Clone as TJSONObject;
+      Users.AddElement(User);
+      Res.Send<TJSONAncestor>(User.Clone).Status(THTTPStatus.Created);
+    end);
+
+  THorse.Delete('/users/:id',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      Id: Integer;
+    begin
+      Id := Req.Params.Items['id'].ToInteger;
+      Users.Remove(Pred(Id)).Free;
+      Res.Send<TJSONAncestor>(Users.Clone).Status(THTTPStatus.NoContent);
+    end);
+
+  THorse.Listen(9000);
 end.
